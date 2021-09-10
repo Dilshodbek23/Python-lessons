47c8c8fca8b8752db6edee9cf39156f0744d4d4a
Dilshodbek23 <61579242+Dilshodbek23@users.noreply.github.com>
2021-09-10 06:11:10 +0500

    Square and cube.
    Function for determining the square and cube of the number.

---
+++ 19_2.py
@@ -0,0 +1,7 @@
+def square_and_cube(number):
+    """Function for determining the square and cube of the number."""
+    print(f"{number}**2={number**2}\
+          {number}**3={number**3}.")
+
+    
+square_and_cube(int(input("Please enter a number: ")))
